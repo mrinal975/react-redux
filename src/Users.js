7e8e302b1559c358c606bc4dc4d8b5f47a3f94fd
mrinal975 <mrinalmallik1@gmail.com>
2022-10-25 11:14:24 +0600

    folder structure

---
+++ src/Users.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+ const Users=(props)=>{
+    const {data} = props;
+   return(
+    <div>
+        <h1>User component</h1>
+        <h4>{data.name}</h4>
+        <h4>{data.age}</h4>
+    </div>
+   ); 
+}
+export default Users;
