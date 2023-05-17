f9544b5ea94c315ebd5dcedbd9f9adccfa89f6e8
Mrinal <mrinalmallik1@gmail.com>
2023-05-17 08:36:19 +0600

    const action reducer created

---
+++ src/services/reducers/todosReducers.js
@@ -0,0 +1,41 @@
+import {
+  GET_TODO_FAILED,
+  GET_TODO_REQUEST,
+  GET_TODO_SUCCESS,
+} from "../constants/todosConstants";
+
+const initialState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case GET_TODO_REQUEST: {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+    case GET_TODO_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        error: false,
+        todos: action.payload,
+      };
+    }
+    case GET_TODO_FAILED: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+export default todosReducer;
